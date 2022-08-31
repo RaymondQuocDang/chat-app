353cd7fdce2b1b875ca22b49fa2bfa0f25c06b02
Raymond Dang <raymondquocdang@gmail.com>
2022-08-31 12:15:55 -0400

    done with basic front-end

--- chat_app_flask/chat-app-react/src/components/Chat.js
+++ chat_app_flask/chat-app-react/src/components/Chat.js
@@ -4,11 +4,11 @@ import { useState, useEffect, useRef } from 'react';
 function Chat() {
 
     const [messageComposerValue, setMessageComposerValue] = useState('');
-    const [messages, setMessages] = useState ([]);
+    const [messages, setMessages] = useState([]);
     const bottomRef = useRef(null);
 
     function sendMessageOnEnter(keyPress) {
-        
+
         if (keyPress === 'Enter') {
             sendMessage();
         }
@@ -21,30 +21,47 @@ function Chat() {
         }
 
         const messageList = messages;
-        messageList.push({'username': 'Newt King', 'message':messageComposerValue});
+        messageList.push({ 'username': 'Newt King', 'message': messageComposerValue });
         setMessages(messageList);
         setMessageComposerValue('')
-    } 
+    }
 
     function displayMessages() {
         const jsxMessages = []
         messages.forEach((message, i) => {
-            jsxMessages.push(
-                <div key={i} className='message-container'>
-                    <p className='username'>{message.username}</p>
-                    <p>{message.message}</p>
-                </div>
-            )
+
+            var prevIndex = i - 1
+            if (i === 0) {
+                jsxMessages.push(
+                    <div key={i} className='message-username-container'>
+                        <p className='username'>{message.username}</p>
+                        <p>{message.message}</p>
+                    </div>
+                )
+            } else if (messages[prevIndex].username === messages[i].username) {
+                jsxMessages.push(
+                    <div key={i} className='message-container'>
+                        <p>{message.message}</p>
+                    </div>
+                )
+            } else {
+                jsxMessages.push(
+                    <div key={i} className='message-container'>
+                        <p className='username'>{message.username}</p>
+                        <p>{message.message}</p>
+                    </div>
+                )
+            }
         });
 
         return jsxMessages;
     }
 
     useEffect(() => {
-        bottomRef.current.scrollIntoView({behavior: 'smooth'});
-      }, [messageComposerValue]);
+        bottomRef.current.scrollIntoView({ behavior: 'smooth' });
+    }, [messageComposerValue]);
 
-    return(
+    return (
         <div className='chat-container'>
             <div className='chat-box'>
                 <div className='welcome-message-container'>
