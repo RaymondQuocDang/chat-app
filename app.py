a9a12d1f46962668e762effc901be7d1264e9cc7
Raymond Dang <raymondquocdang@gmail.com>
2022-09-07 17:24:06 -0400

    removed error handler

--- app.py
+++ app.py
@@ -119,10 +119,5 @@ def connected():
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
-@app.errorhandler(404)   
-def not_found(e):   
-  return app.send_static_file('index.html')
-
-
 if __name__ == '__main__':
     socketio.run(app, debug=True)
